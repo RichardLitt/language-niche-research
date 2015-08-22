fef11174163710cc0556917810a3639081234802
Richard Littauer <richard.littauer@gmail.com>
2015-08-22 13:51:10 -0400

    Getting FreqDist

--- ngrams.py
+++ ngrams.py
@@ -1,11 +1,26 @@
+import nltk
+from nltk import bigrams
+from nltk import trigrams
 from nltk.util import ngrams
 from sys import argv
 script, filename = argv
 
-txt = open(filename)
-n = 6
+text = open(filename)
 
-for line in txt:
-  sixgrams = ngrams(line.split(), n)
-  for grams in sixgrams:
-    print grams
+for n in range (2,7):
+  for line in text:
+    tokens = nltk.word_tokenize(line)
+    tokens = [token.lower() for token in tokens if len(token) > 1] #same as unigrams
+    grams = ngrams(tokens, n)
+    fdist = nltk.FreqDist(grams)
+    print fdist.most_common()
+  # for k,v in fdist.items():
+  #    print v, k
+  # print [(item, tri_tokens.count(item)) for item in sorted(set(tri_tokens))]
+
+
+# ngrams
+# for n in range(0,6):
+#   for line in text:
+#     for grams in ngrams(line.split(), n):
+#       print grams
