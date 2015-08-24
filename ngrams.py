ecae97ac8ce3fc3840a5091c9b0c04f8bdd53b17
Richard Littauer <richard.littauer@gmail.com>
2015-08-24 12:00:07 -0400

    Attempt at using bigrams and trigrams from https://stackoverflow.com/questions/12821201/what-are-ngram-counts-and-how-to-implement-using-nltk, but fails due to generator count error I don't understand

--- ngrams.py
+++ ngrams.py
@@ -7,16 +7,17 @@ script, filename = argv
 
 text = open(filename)
 
-for n in range (2,7):
-  for line in text:
-    tokens = nltk.word_tokenize(line)
-    tokens = [token.lower() for token in tokens if len(token) > 1] #same as unigrams
-    grams = ngrams(tokens, n)
-    fdist = nltk.FreqDist(grams)
-    print fdist.most_common()
+# for n in range (2,7):
+for line in text:
+  tokens = nltk.word_tokenize(line)
+  tokens = [token.lower() for token in tokens if len(token) > 1] #same as unigrams
+  bi_tokens = bigrams(tokens)
+  tri_tokens = trigrams(tokens)
+  print [(item, tri_tokens.count(item)) for item in sorted(set(tri_tokens))]
+    # fdist = nltk.FreqDist(grams)
+    # print fdist.most_common()
   # for k,v in fdist.items():
   #    print v, k
-  # print [(item, tri_tokens.count(item)) for item in sorted(set(tri_tokens))]
 
 
 # ngrams
